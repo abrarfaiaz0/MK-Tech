ef7057f93a5c714e3aae157b66e82f606c20cf65
abrarfaiazatuno <abrarfaiaz0@gmail.com>
2022-06-07 17:33:35 +0600

    finalized

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const banner = document.getElementById("banner");
 const native = document.getElementById("native");
 const skim = document.getElementById("skim");
 
+const left = document.getElementById("slider-left");
+const right = document.getElementById("slider-right");
+
+let activeId = 1;
+
 pop.onclick = function(){goPop();}
 banner.onclick = function(){goBanner();}
 native.onclick = function(){goNative();}
 skim.onclick = function(){goSkim();}
+
+right.onclick = function(){slideRight(activeId);}
+left.onclick = function(){slideLeft(activeId);}
 
 
 function goPop(){
@@ -14,8 +22,10 @@ function goPop(){
     let button = document.getElementById("pop");
    let title = document.getElementById("ad-title");
    let text = document.getElementById("ad-text");
+   let img = document.getElementById("ad-img");
    button.style.color = "white";
    button.style.backgroundColor = "#FF6F00";
+   img.src = "src/purple-1.png";
    title.innerHTML = "POP-UNDER";
    text.innerHTML = "This is one of the most popular ads. After user’s click, this ad opens landing page in behind. So, the user experience is not hampered much.";
 }
@@ -24,8 +34,10 @@ function goNative(){
     let button = document.getElementById("native");
    let title = document.getElementById("ad-title");
    let text = document.getElementById("ad-text");
+   let img = document.getElementById("ad-img");
    button.style.color = "white";
    button.style.backgroundColor = "#FF6F00";
+   img.src = "src/purple-3.png";
    title.innerHTML = "NATIVE";
    text.innerHTML = "Ad format with the image and the title, that perfectly fits into the content of the site. Native ads are placed in the most visible areas of the page and, thus, this format shows good results and the level of interaction.";
 }
@@ -34,6 +46,8 @@ function goSkim(){
     let button = document.getElementById("skim");
    let title = document.getElementById("ad-title");
    let text = document.getElementById("ad-text");
+   let img = document.getElementById("ad-img");
+   img.src = "src/purple-4.png";
    button.style.color = "white";
    button.style.backgroundColor = "#FF6F00";
    title.innerHTML = "SKIM";
@@ -44,6 +58,8 @@ function goBanner(){
     let button = document.getElementById("banner");
    let title = document.getElementById("ad-title");
    let text = document.getElementById("ad-text");
+   let img = document.getElementById("ad-img");
+   img.src = "src/purple-2.png";
    button.style.color = "white";
    button.style.backgroundColor = "#FF6F00";
    title.innerHTML = "BANNER AD";
@@ -59,4 +75,42 @@ function clearOther(){
     banner.style.backgroundColor = "#F2F2F2";
     native.style.backgroundColor = "#F2F2F2";
     skim.style.backgroundColor = "#F2F2F2";
+}
+
+function sliderClear()
+{
+    document.getElementById("1").classList.remove("slider-menu-active");
+    document.getElementById("2").classList.remove("slider-menu-active");
+    document.getElementById("3").classList.remove("slider-menu-active");
+    document.getElementById("4").classList.remove("slider-menu-active");
+}
+
+function slideRight(n){
+    sliderClear();
+    if(activeId!=4){
+    n = n+1;
+    activeId = n + "";}
+    else activeId = "1";
+    let active = document.getElementById(activeId);
+    active.classList.add("slider-menu-active");
+    let activeT = document.getElementById("sld-txt-1");
+    activeT.innerHTML = "Title "+activeId;
+    let activeSlideText = document.getElementById("sld-imgt");
+    activeSlideText.innerHTML = "" + activeId;
+    activeId = parseInt(activeId);
+}
+function slideLeft(n){
+    sliderClear();
+    if(activeId!=1){
+    n = n-1;
+    activeId = n + "";}
+    else activeId = "4";
+    let active = document.getElementById(activeId);
+    active.classList.add("slider-menu-active");
+    let activeT = document.getElementById("sld-txt-1");
+    activeT.innerHTML = "Title "+activeId;
+    let activeSlideText = document.getElementById("sld-imgt");
+    activeSlideText.innerHTML = "" + activeId;
+    activeId = parseInt(activeId);
+    
 }
